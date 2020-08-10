8bc83fe8ed12581382f402888b963d907529c287
Jeremy Kuhnash <kuhnash@gmail.com>
2020-08-10 02:40:52 -0400

    build script for pushing images

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -ex
+
+# This script is the Jenkins build helper as it were. 
+
+GIT_SHA=$(git rev-parse HEAD | cut -c 1-8)
+eval $(/usr/local/bin/aws ecr get-login --no-include-email --profile=ecr-user --region=us-east-2)
+
+IMAGE="jeremykuhnash/targetd:$GIT_SHA"
+docker build -t $IMAGE -f docker/Dockerfile .
+docker push $IMAGE
